65faee916435f583a3d544f81528379b308bfb7a
Zakhar Voit <zakharvoit@gmail.com>
2014-07-27 13:06:22 +1100

    Added runner for debigging

---
+++ src/lisp/runner.lisp
@@ -0,0 +1,21 @@
+(defun runner ()
+  (step 0 (world))
+  )
+
+(defun world ()
+  (cons (field) (cons (lambdaman) 0))
+  )
+
+(defun lambdaman ()
+  (cons 3 (cons (cons 2 1) 0))
+  )
+
+;; [ [ 0, 0, 0 ]
+;; , [ 0, 2, 2 ]
+;; ]
+(defun field ()
+  (cons (cons 0 (cons 0 (cons 0 0)))
+        (cons (cons 0 (cons 2 (cons 2 0)))
+              0)
+        )
+  )
